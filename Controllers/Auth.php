18996743b46e3d366ca80ad509614f6694a556d4
tatev-al <tatevik.aleksanyan19@gmail.com>
2021-04-08 07:46:29 +0400

    Autoload

---
+++ Controllers/Auth.php
@@ -0,0 +1,13 @@
+<?php
+
+class Auth
+{
+    function test()
+    {
+          echo 'test \'Auth\' class';
+    }
+    function index()
+    {
+        echo 'index method from \'Auth\' class';
+    }
+}
