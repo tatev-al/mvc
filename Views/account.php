590b6e54697ba16a4f225b98e04fe718eee24fe8
tatev-al <tatevik.aleksanyan19@gmail.com>
2021-04-19 12:56:48 +0400

    Friends and other changes

--- Views/account.php
+++ Views/account.php
@@ -1,21 +1,23 @@
 <div class = "d-flex bd-highlight mb-3">
-<div class="p-2 bd-highlight">
-    <img src="/Public/Images/Avatars/<?= $this->accountData['avatar_img'] ?>" class="img-fluid img-thumbnail " width='180' height='180' alt="avatar">
-</div>
-    <div class="p-2 bd-highlight">     
+    <div class="col-sm-3">
+        <img src="/Public/Images/Avatars/<?= $this->accountData['avatar_img'] ?>" class="img-fluid img-thumbnail " width='180' height='180' alt="avatar">
+        <?php if($_SESSION['id'] == $this->accountData['id'])
+        {?>
+            <div class="custom-file">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <input type="file" id="avatar_img" name="avatar_img" class = 'custom-file-input' onchange="this.form.submit();">
+                    <label class="custom-file-label" for='avatar_img'>Change Avatar Photo</label>
+                    
+                    <small id="imgWarning" class="form-text text-danger"><?= $this->imgUploadError ?></small>
+                    <small id="imgWarning" class="form-text text-danger"><?= $this->imgTypeError ?></small>
+                    <small id="imgWarning" class="form-text text-danger"><?= $this->imgSizeError ?></small>
+                    <small id="imgWarning" class="form-text text-danger"><?= $this->imgExistError ?></small>
+                </form>
+            </div>
+        <?php } ?>
+    </div>
+    <div class='col-sm-3'>     
         <h2 class="d-flex justify-content-center mt-2">Personal Page <?= $this->accountData['name'] ?></h2>
     </div>
 
-</div>    
-<?php if($_SESSION['id'] == $this->accountData['id'])
-{?>
-<div class="block col-sm-3">
-    <form action="" method="post" enctype="multipart/form-data"> 
-        <input type="file" id="avatar_img" name="avatar_img" onchange="this.form.submit();">
-        <small id="imgWarning" class="form-text text-danger"><?= $this->imgUploadError ?></small>
-        <small id="imgWarning" class="form-text text-danger"><?= $this->imgTypeError ?></small>
-        <small id="imgWarning" class="form-text text-danger"><?= $this->imgSizeError ?></small>
-        <small id="imgWarning" class="form-text text-danger"><?= $this->imgExistError ?></small>
-    </form>
 </div>
-<?php } ?>
