30781b6e00655a95281d69c25616ef39176cb9ca
tatev-al <tatevik.aleksanyan19@gmail.com>
2021-04-19 12:48:35 +0400

    Friends and other changes

--- Controllers/Account.php
+++ Controllers/Account.php
@@ -53,7 +53,7 @@ class Account extends Controller
             }
         }
         $this->view->accountData = $user->getUserById($_SESSION['id']);
-        if(empty($this->view->accountData) || !file_exists($this->view->accountData['avatar_img']))
+        if(empty($this->view->accountData))
         {
             $user->uploadAvatar('avatar.png');
             $this->view->accountData['avatar_img'] = 'avatar.png';
